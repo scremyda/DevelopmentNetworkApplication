fa5fad99d707540433f7300d9490a0536073bcdd
Belozerovmsk <belozerov.msk@gmail.com>
2024-01-20 22:58:02 +0300

    save search field

--- backend/internal/api/server.go
+++ backend/internal/api/server.go
@@ -17,7 +17,7 @@ func StartServer() {
 	autoparts := []backend.Autoparts{
 		{1, "Зарядный кабель", "Прицепное устройство Bosal, под фаркоп, в сборе с розеткой, Tesla Model X.", "image11.jpeg", []string{"1027581-00-D", "1046032-00-C", "10057987-001"}},
 		{2, "Ротор электромотора", "Ротор электромотора, лев./прав., Tesla Model X.", "image22.jpeg", []string{"1285161-00-C", "1027161-00-C", "1046032-00-C"}},
-		{3, "Электродвигатель", "Электродвигатель в сборе (теплообменник, бачок расширительный, трубки фреона, клапана, электропроводка), Tesla Model 3, Y.", "image33.jpg", []string{"1547595-96-F", ", 1523000-96-E", "1523001-00-D"}},
+		{3, "Электродвигатель", "Электродвигатель в сборе (теплообменник, бачок расширительный, трубки фреона, клапана, электропроводка), Tesla Model 3, Y.", "image33.jpg", []string{"1547595-96-F", "1523000-96-E", "1523001-00-D"}},
 	}
 
 	r.LoadHTMLGlob("templates/*")
@@ -26,6 +26,7 @@ func StartServer() {
 		r.SetHTMLTemplate(template.Must(template.ParseFiles("templates/index.tmpl")))
 		searchQuery := c.Query("search")
 		filteredautoparts := []backend.Autoparts{}
+
 		if searchQuery != "" {
 			for _, autopart := range autoparts {
 				if strings.Contains(strings.ToLower(autopart.Name), strings.ToLower(searchQuery)) {
@@ -35,12 +36,13 @@ func StartServer() {
 
 			c.HTML(http.StatusOK, "index.tmpl", gin.H{
 				"Autoparts":   filteredautoparts,
-				"SearchValue": searchQuery, // добавлено здесь
+				"SearchValue": searchQuery,
 			})
 
 		} else {
 			c.HTML(http.StatusOK, "index.tmpl", gin.H{
-				"Autoparts": autoparts,
+				"Autoparts":   autoparts,
+				"SearchValue": searchQuery,
 			})
 		}
 	})
@@ -57,7 +59,8 @@ func StartServer() {
 		}
 
 		c.HTML(http.StatusOK, "info.tmpl", gin.H{
-			"Autoparts": selectedautopart,
+			"Autoparts":   selectedautopart,
+			"SearchValue": c.Query("search"), // Передаем search в шаблон info.tmpl
 		})
 	})
 
